f568f9160deb1883044cafff550fa24bda57be63
Zulkuf Demirhan <z.demirhan95@gmail.com>
2023-07-11 22:28:40 +0300

    delete function created

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,12 @@ export default function App() {
     })
   }
 
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -44,6 +50,7 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
           return (
             <li key={todo.id}>
@@ -51,7 +58,7 @@ export default function App() {
                 <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
                 {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
             </li>
           )
         })}
